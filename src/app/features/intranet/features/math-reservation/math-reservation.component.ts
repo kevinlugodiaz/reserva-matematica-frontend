c611c866f25655c1e4661a3c389fb98c86f4162d
Kevin Lugo <kevinlugodev@users.noreply.github.com>
2025-07-04 11:25:42 -0500

    upd(reservas): avance tab 1

--- src/app/features/intranet/features/math-reservation/math-reservation.component.ts
+++ src/app/features/intranet/features/math-reservation/math-reservation.component.ts
@@ -12,103 +12,103 @@ import ClosingReportsComponent from './tabs/closing-reports/closing-reports.comp
 import { Button } from 'primeng/button';
 
 @Component({
-  selector: 'app-math-reservation',
-  imports: [
-    TabsModule,
-    TabList,
-    Tabs,
-    Tab,
-    TabPanels,
-    TabPanel,
-    GenInfoComponent,
-    GenReservationInterfacesComponent,
-    FeedbackProcessComponent,
-    ClosingResultComponent,
-    AccountingReconciliationComponent,
-    ClosingReportsComponent,
-    Toolbar,
-    Select,
-    Button
-  ],
-  standalone: true,
-  templateUrl: './math-reservation.component.html',
-  styleUrl: './math-reservation.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-math-reservation',
+	imports: [
+		TabsModule,
+		TabList,
+		Tabs,
+		Tab,
+		TabPanels,
+		TabPanel,
+		GenInfoComponent,
+		GenReservationInterfacesComponent,
+		FeedbackProcessComponent,
+		ClosingResultComponent,
+		AccountingReconciliationComponent,
+		ClosingReportsComponent,
+		Toolbar,
+		Select,
+		Button
+	],
+	standalone: true,
+	templateUrl: './math-reservation.component.html',
+	styleUrl: './math-reservation.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class MathReservationComponent {
 
-  productsList = signal([
-    {
-      label: 'Renta Vitalicia',
-      value: 'renta-vitalicia'
-    }
-  ]);
-  yearsList = signal([
-    {
-      label: 2025,
-      value: 2025
-    },
-    {
-      label: 2024,
-      value: 2024
-    },
-    {
-      label: 2023,
-      value: 2023
-    },
-    {
-      label: 2022,
-      value: 2022
-    }
-  ]);
-  monthsList = signal([
-    {
-      label: 'Enero',
-      value: 1
-    },
-    {
-      label: 'Febrero',
-      value: 2
-    },
-    {
-      label: 'Marzo',
-      value: 3
-    },
-    {
-      label: 'Abril',
-      value: 4
-    },
-    {
-      label: 'Mayo',
-      value: 5
-    },
-    {
-      label: 'Junio',
-      value: 6
-    },
-    {
-      label: 'Julio',
-      value: 7
-    },
-    {
-      label: 'Agosto',
-      value: 8
-    },
-    {
-      label: 'Septiembre',
-      value: 9
-    },
-    {
-      label: 'Octubre',
-      value: 10
-    },
-    {
-      label: 'Noviembre',
-      value: 11
-    },
-    {
-      label: 'Diciembre',
-      value: 12
-    }
-  ]);
+	productsList = signal([
+		{
+			label: 'Renta Vitalicia',
+			value: 'renta-vitalicia'
+		}
+	]);
+	yearsList = signal([
+		{
+			label: 2025,
+			value: 2025
+		},
+		{
+			label: 2024,
+			value: 2024
+		},
+		{
+			label: 2023,
+			value: 2023
+		},
+		{
+			label: 2022,
+			value: 2022
+		}
+	]);
+	monthsList = signal([
+		{
+			label: 'Enero',
+			value: 1
+		},
+		{
+			label: 'Febrero',
+			value: 2
+		},
+		{
+			label: 'Marzo',
+			value: 3
+		},
+		{
+			label: 'Abril',
+			value: 4
+		},
+		{
+			label: 'Mayo',
+			value: 5
+		},
+		{
+			label: 'Junio',
+			value: 6
+		},
+		{
+			label: 'Julio',
+			value: 7
+		},
+		{
+			label: 'Agosto',
+			value: 8
+		},
+		{
+			label: 'Septiembre',
+			value: 9
+		},
+		{
+			label: 'Octubre',
+			value: 10
+		},
+		{
+			label: 'Noviembre',
+			value: 11
+		},
+		{
+			label: 'Diciembre',
+			value: 12
+		}
+	]);
 }
--- src/app/features/intranet/features/math-reservation/tabs/gen-info/nested/report-gen/report-gen.component.ts
+++ src/app/features/intranet/features/math-reservation/tabs/gen-info/nested/report-gen/report-gen.component.ts
@@ -6,42 +6,70 @@ import { Tag } from 'primeng/tag';
 import { Tooltip } from 'primeng/tooltip';
 import { DatePipe, TitleCasePipe } from '@angular/common';
 
+interface Report {
+	id: number;
+	product: string;
+	year: number;
+	month: string;
+	createdAt: Date;
+	status: {
+		code: string;
+		label: string;
+		severity: string;
+	};
+}
+
 @Component({
-  selector: 'app-report-gen',
-  imports: [
-    Button,
-    TableModule,
-    Message,
-    Tag,
-    Tooltip,
-    DatePipe,
-    TitleCasePipe
-  ],
-  standalone: true,
-  templateUrl: './report-gen.component.html',
-  styleUrl: './report-gen.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-report-gen',
+	imports: [
+		Button,
+		TableModule,
+		Message,
+		Tag,
+		Tooltip,
+		DatePipe,
+		TitleCasePipe
+	],
+	standalone: true,
+	templateUrl: './report-gen.component.html',
+	styleUrl: './report-gen.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class ReportGenComponent {
-  readonly dataSource = signal<any[]>([]);
+	readonly dataSource = signal<Report[]>([]);
 
-  newReport(): void {
-    this.dataSource.update((currentValue) => {
-      return [
-        ...currentValue,
-        {
-          id: currentValue.length + 1,
-          product: 'Renta Vitalicia',
-          year: 2025,
-          month: 'Enero',
-          createdAt: new Date(),
-          status: {
-            code: 'FINISHED',
-            label: 'Carga Finalizada',
-            severity: 'success'
-          }
-        }
-      ];
-    });
-  }
+	newReport(): void {
+		this.dataSource.update((currentValue) => {
+			return [
+				...currentValue,
+				{
+					id: currentValue.length + 1,
+					product: 'Renta Vitalicia',
+					year: 2025,
+					month: 'Enero',
+					createdAt: new Date(),
+					status: {
+						code: 'IN_PROGRESS',
+						label: 'Carga Iniciada',
+						severity: 'secondary'
+					}
+				}
+			];
+		});
+
+		setTimeout(() => {
+			this.dataSource.update(currentValue => {
+				return currentValue.map((item) => {
+					return {
+						...item,
+						status: {
+							code: 'FINISHED',
+							label: 'Carga Finalizada',
+							severity: 'success'
+						}
+					};
+				});
+			});
+		}, 4000);
+	}
 }
